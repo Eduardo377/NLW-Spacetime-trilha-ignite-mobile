14e2862213480afe7f5c08d1877b0400cd5315c0
Eduardo377 <eduardogomes377@gmail.com>
2023-05-19 17:40:17 -0300

    fix: correcao de bugs

--- App.tsx
+++ App.tsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, View, Text, TouchableOpacity } from "react-native";
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
+} from "@expo-google-fonts/roboto";
 
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import blurBg from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg";
+import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
+import { styled } from "nativewind";
 
-const StyleStripes = styled(Stripes)
-const StyleNLWLogo = styled(NLWLogo)
+const StyleStripes = styled(Stripes);
+const StyleNLWLogo = styled(NLWLogo);
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
-  })
+  });
 
   if (!hasLoadedFonts) {
-    return null
+    return null;
   }
 
   return (
     <ImageBackground
       source={blurBg}
       className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
+      imageStyle={{ position: "absolute", left: "-100%" }}
     >
       <StyleStripes className="absolute left-2" />
 
@@ -63,5 +63,5 @@ export default function App() {
       </Text>
       <StatusBar style="light" backgroundColor="gray" />
     </ImageBackground>
-  )
+  );
 }
