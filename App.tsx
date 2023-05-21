3613ab7c3e8299a26474a1a5285ace8f56d319c5
Eduardo377 <eduardogomes377@gmail.com>
2023-05-20 23:15:18 -0300

    feature: auteticacao

--- App.tsx
+++ App.tsx
@@ -1,38 +1,69 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
+} from '@expo-google-fonts/roboto'
 
-import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-import blurBg from "./src/assets/bg-blur.png";
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
-import { styled } from "nativewind";
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind'
+import {
+  useAuthRequest,
+  makeRedirectUri /*, AuthSession */,
+} from 'expo-auth-session'
 
-const StyleStripes = styled(Stripes);
-const StyleNLWLogo = styled(NLWLogo);
+const StyleStripes = styled(Stripes)
+const StyleNLWLogo = styled(NLWLogo)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/3bcb396f49f968e6dc68',
+}
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
-  });
+  })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '3bcb396f49f968e6dc68',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    console.log(response)
+    console.log(request)
+    if (response?.type === 'success') {
+      const { code } = response.params
+      console.log(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
-    return null;
+    return null
   }
 
   return (
     <ImageBackground
       source={blurBg}
       className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: "absolute", left: "-100%" }}
+      imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyleStripes className="absolute left-2" />
 
@@ -52,6 +83,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembraça
@@ -63,5 +95,5 @@ export default function App() {
       </Text>
       <StatusBar style="light" backgroundColor="gray" />
     </ImageBackground>
-  );
+  )
 }
